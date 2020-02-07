f1651e76f795d7e586f8c8772c405f4eef8a3ff2
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2020-02-07 17:06:54 +0300

    Consider the file to be encrypted only if it has encrypted property set to tue

--- lib/private/Files/Storage/Wrapper/Encryption.php
+++ lib/private/Files/Storage/Wrapper/Encryption.php
@@ -392,8 +392,13 @@ class Encryption extends Wrapper {
 				// specified in the file header - otherwise we need to fail hard to
 				// prevent data loss on client side
 				if (!empty($encryptionModuleId)) {
-					$targetIsEncrypted = true;
-					$encryptionModule = $this->encryptionManager->getEncryptionModule($encryptionModuleId);
+					$data = $this->storage->getCache()->get($path);
+					if ($data) {
+						$targetIsEncrypted = $data->isEncrypted();
+					}
+					if ($targetIsEncrypted) {
+						$encryptionModule = $this->encryptionManager->getEncryptionModule($encryptionModuleId);
+					}
 				}
 
 				if ($this->file_exists($path)) {
